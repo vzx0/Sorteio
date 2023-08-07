a35dc5779fa19445407451623669eb44007ac5f8
Victor Zanchetta Xisto <133662176+victorzx06@users.noreply.github.com>
2023-08-07 14:59:00 -0300

    Atividade 07.08

---
+++ sorteio/aula.js
@@ -0,0 +1,17 @@
+//Math.random gera um numero aleatorio
+console.log(Math.random());
+
+// Gerando um número aleatório entre 0 e 20
+console.log(Math.random() * 20);
+
+// Math.round arredonda para o inteiro mais próximo
+console.log(Math.round(3.1));
+console.log(Math.round(3.9));
+
+// Math.ceil arredonda para cima
+console.log(Math.ceil(3.1));
+console.log(Math.ceil(3.9));
+
+// Math.floor arredonda para baixo
+console.log(Math.floor(3.1));
+console.log(Math.floor(3.9));
